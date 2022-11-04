dc85eddda156ab1db6abad71ab95d780f4486efa
DanielGRasmussen <104102130+DanielGRasmussen@users.noreply.github.com>
2022-11-04 11:18:33 -0700

    Added stuff to make pressableButtons work from main.py and function to add button.

--- PressableButtons.py
+++ PressableButtons.py
@@ -3,49 +3,54 @@ import arcade.gui
 
 
 class PressableButtons:
-    def __init__(self):
-        self.uimanager = arcade.gui.UIManager
-        self.uimanager.enable()
+    def __init__(self, uimanager):
+        self.uimanager = uimanager
 
-        # Create the three buttons for the three options the user can choose from
-        first_button = arcade.gui.UIFlatButton(TEXT="", WIDTH=100)
-        second_button = arcade.gui.UIFlatButton(TEXT="", WIDTH=100)
-        third_button = arcade.gui.UIFlatButton(TEXT="", WIDTH=100)
+        self.add_button("center_x", 250, "top", -100)
+        self.add_button("center_x", 0, "top", -100)
+        self.add_button("center_x", -250, "top", -100)
+
+        self.draw()
+    
+    def add_button(self, anchor_x, align_x, anchor_y, align_y):
+        button = arcade.gui.UIFlatButton(TEXT="", WIDTH=100)
+
+        button_box = arcade.gui.UIBoxLayout() # Create a box group to align the 'open' button in the center
+        button_box.add(button) # Create a button. We'll click on this to open our window.
+        button.on_click = self.on_click_open # Add a hook to run when we click on the button.
 
         self.uimanager.add(
             arcade.gui.UIAnchorWidget(
                 # Positioning for first option
-                anchor_x="center_x",
-                anchor_y="center_y",
+                anchor_x=anchor_x,
+                align_x=align_x,
+                anchor_y=anchor_y,
+                align_y=align_y,
 
-                child=first_button)
+                child=button
             )
-
-        self.uimanager.add(
-            arcade.gui.UIAnchorWidget(
-                # Positioning for second option
-                anchor_x="center_x",
-                anchor_y="center_y",
-
-                child=second_button)
+        )
+    
+    def on_click_open(self, event):
+        # The code in this function is run when we click the ok button.
+        # The code below opens the message box and auto-dismisses it when done.
+        message_box = arcade.gui.UIMessageBox(
+            width=375,
+            height=250,
+            message_text=(
+                "This is a Help Message box for giving users in game hints "
+                "click ok to close."
+            ),
+            callback=self.on_message_box_close,
+            buttons=["Ok", "Cancel", "Test"]
         )
 
-        self.uimanager.add(
-            arcade.gui.UIAnchorWidget(
-                # Positioning for third option
-                anchor_x="center_x",
-                anchor_y="center_y",
+        self.uimanager.add(message_box)
+    
+    def on_message_box_close(self, button_text):
+        print(f"User pressed {button_text}.")
 
-                child=third_button)
-        )
+    def draw(self):
+        arcade.start_render()
 
-
-# Handles button click event when any of the three options are chosen.
-def button_click(self, event):
-    print("You clicked a button!")
-
-
-def draw(self):
-    arcade.start_render()
-
-    self.uimanager.draw()
+        self.uimanager.draw()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import arcade
 import arcade.gui
+from PressableButtons import PressableButtons
 
 
 class MyWindow(arcade.Window):
@@ -12,47 +13,11 @@ class MyWindow(arcade.Window):
         self.manager = arcade.gui.UIManager()
         self.manager.enable()
 
-        # Create a box group to align the 'open' button in the center
-        self.v_box = arcade.gui.UIBoxLayout()
-
-        # Create a button. We'll click on this to open our window.
-        # Add it v_box for positioning.
-        open_message_box_button = arcade.gui.UIFlatButton(text="Open", width=250)
-        self.v_box.add(open_message_box_button)
-
-        # Add a hook to run when we click on the button.
-        open_message_box_button.on_click = self.on_click_open
-        # Create a widget to hold the v_box widget, that will center the buttons
-        self.manager.add(
-            arcade.gui.UIAnchorWidget(
-                anchor_x="center_x",
-                anchor_y="center_y",
-                child=self.v_box)
-        )
-
-    def on_click_open(self, event):
-        # The code in this function is run when we click the ok button.
-        # The code below opens the message box and auto-dismisses it when done.
-        message_box = arcade.gui.UIMessageBox(
-            width=375,
-            height=250,
-            message_text=(
-                "This is a Help Message box for giving users in game hints "
-                "click ok to close."
-            ),
-            callback=self.on_message_box_close,
-            buttons=["Ok", "Cancel", "Test"]
-        )
-
-        self.manager.add(message_box)
+        buttons = PressableButtons(self.manager)
 
     def on_draw(self):
         self.clear()
         self.manager.draw()
-
-    def on_message_box_close(self, button_text):
-        print(f"User pressed {button_text}.")
-
 
 window = MyWindow()
 arcade.run()
