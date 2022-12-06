6b02bfef036a828e5d36e9252b3d1fb99eeca53d
DanielGRasmussen <104102130+DanielGRasmussen@users.noreply.github.com>
2022-12-06 14:55:54 -0700

    Created some better structure listing for main view.

--- main.py
+++ main.py
@@ -9,20 +9,38 @@ from views.multiplication import Multiplication
 
 class HomeView(arcade.View):
     def __init__(self):
+        """Main view, it is where you can navigate the actual game.
+        
+        Starts by calling draw_board which makes background colors and general stuff in it then calls draw_buttons
+            draw_buttons draws each button and attaches their individual functions for when they are clicked
+        
+        Variables:
+            manager (UIManager): Instance of arcade's UI Manager
+            difficulty (string): Will either be Easy, Normal, or Hard to determine the difficulty
+        """
         super().__init__()
-
-        # Create and enable the UIManager
+        self.difficulty = "Normal"
+        self.draw_board()
+    
+    def draw_board(self):
+        """Draws the entire screen and creates the question"""
+        # Set a background color
+        arcade.set_background_color(arcade.color.LIGHT_CORNFLOWER_BLUE)
+        
+        self.clear()
         self.manager = arcade.gui.UIManager()
         self.manager.enable()
-        self.difficulty = "Normal"
-
-        arcade.set_background_color(arcade.color.LIGHT_CORNFLOWER_BLUE)
-
+                
+        self.draw_buttons() # Creates and draws the buttons
+    
+    def draw_buttons(self):
+        """Creates and draws the buttons"""
         help_button = arcade.gui.UIFlatButton(text="HELP", width=200)
         difficulty_button = arcade.gui.UIFlatButton(text="DIFFICULTY", width=200)
         play_button = arcade.gui.UIFlatButton(text="PLAY", width=200)
 
-        help_button.on_click = self.on_click_help  # Add a hook to run when we click on the button.
+        # Attaches their individual function to them
+        help_button.on_click = self.on_click_help
         difficulty_button.on_click = self.on_click_difficulty
         play_button.on_click = self.on_click_play
 
@@ -45,34 +63,24 @@ class HomeView(arcade.View):
                 )
             )
 
-    def on_click_help(self, event):
-        # The code in this function is run when we click the ok button.
-        # The code below opens the message box and auto-dismisses it when done.
-        message_box = arcade.gui.UIMessageBox(
-            width=375,
-            height=250,
-            message_text=(
-                "This is a Help Message box for giving users in game hints "
-                "click ok to close."
-            ),
-            callback=self.on_message_box_close,
-            buttons=["Ok", "Cancel"]
-        )
+    def on_click_play(self, _):
+        """Gets called when the user clicks the play button, starts the game depending on difficulty"""
+        if self.difficulty == "Easy":
+            game_view = Matching()
+        elif self.difficulty == "Normal":
+            game_view = Addition()
+        else:
+            game_view = Multiplication()
+        self.window.show_view(game_view)
 
-        self.manager.add(message_box)
-
-    def on_message_box_close(self, button_text):
-        print(f"User pressed {button_text}.")
-
-    def on_click_difficulty(self, event):
-        # The code in this function is run when we click the ok button.
+    def on_click_difficulty(self, _):
+        """Gets called when the user clicks the difficulty button, shows options and let's them pick"""
         # The code below opens the message box and auto-dismisses it when done.
         message_box = arcade.gui.UIMessageBox(
             width=350,
             height=125,
             message_text=(
                 "Select the difficulty you want to play at."
-
             ),
             callback=self.on_difficulty_box_close,
             buttons=["Easy", "Normal", "Hard"]
@@ -80,21 +88,32 @@ class HomeView(arcade.View):
 
         self.manager.add(message_box)
 
+    def on_click_help(self, _):
+        """Gets called when the user clicks the help button, shows a description of the game"""
+        # The code below opens the message box and auto-dismisses it when done.
+        message_box = arcade.gui.UIMessageBox(
+            width=375,
+            height=250,
+            message_text=(
+                "This is a Help Message box for giving users in game hints."
+                "Click Ok to close."
+            ),
+            buttons=["Ok"]
+        )
+
+        self.manager.add(message_box)
+
     def on_difficulty_box_close(self, button_text):
+        """Gets called when the difficulty box closes, sets the difficulty based on the button pressed
+        
+        Arguments:
+            button_text (string): The text for the button pressed
+        """
         self.difficulty = button_text
         print(self.difficulty)
 
-    def on_click_play(self, event):
-        difficulty = self.difficulty
-        if difficulty == "Easy":
-            game_view = Matching()
-        elif difficulty == "Normal":
-            game_view = Addition()
-        else:
-            game_view = Multiplication()
-        self.window.show_view(game_view)
-
     def on_draw(self):
+        """Starts the drawing process"""
         self.clear()
         self.manager.draw()
 
@@ -103,12 +122,8 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Ducks Matching Game"
 
-def main():
-    """ Main function """
-
-    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, resizable=True)
+if __name__ == "__main__":
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, resizable=False)
     start_view = HomeView()
     window.show_view(start_view)
     arcade.run()
-
-main()
